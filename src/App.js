c76aed3e64706cc1013beb076926303234289483
alisyabob <sjs939883@gmail.com>
2021-07-04 23:51:39 +0900

    고객 컴포넌트 구조화하기

--- src/App.js
+++ src/App.js
@@ -1,24 +1,57 @@
 import React, { Component } from 'react';
 import Customer from './components/Customer'
 import './App.css';
+import { isCompositeComponent } from 'react-dom/test-utils';
 
-const customer = {
-  'name': '홍길동',
-  'birthday': '940000',
+const customers = [{
+  'id': 1,
+  'image': 'https://placeimg.com/64/64/1',
+  'name': '홍길동1',
+  'birthday': '941000',
+  'gender': '남자',
+  'job': '대학생'
+
+},
+{
+  'id': 2,
+  'image': 'https://placeimg.com/64/64/2',
+  'name': '홍길동2',
+  'birthday': '940200',
+  'gender': '남자',
+  'job': '대학생'
+
+},
+{
+  'id': 3,
+  'image': 'https://placeimg.com/64/64/3',
+  'name': '홍길동3',
+  'birthday': '940300',
   'gender': '남자',
   'job': '대학생'
 
 }
+]
 
 class App extends Component {
   render() {
     return (
-      <Customer
-        name={customer.name}
-        birthday={customer.birthday}
-        gender={customer.gender}
-        job={customer.job}
-      />
+      <div>
+        {
+          customers.map(c => {
+            return (
+              <Customer
+                key={c.id}
+                id={c.id}
+                image={c.image}
+                name={c.name}
+                birthday={c.birthday}
+                gender={c.gender}
+                job={c.job}
+              />
+            );
+          })
+        }
+      </div>
     );
   }
 }
