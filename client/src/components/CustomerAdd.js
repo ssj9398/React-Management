18c7be1bc0b715c2492cf8d020fbde5ec8846a31
alisyabob <sjs939883@gmail.com>
2021-07-25 23:53:53 +0900

    add fileupload & database

--- client/src/components/CustomerAdd.js
+++ client/src/components/CustomerAdd.js
@@ -21,6 +21,15 @@ class CustomerAdd extends React.Component {
             .then((response) => {
                 console.log(response.data);
             })
+        this.setState({
+            file: null,
+            userName: '',
+            birthday: '',
+            gender: '',
+            job: '',
+            fileName: ''
+        })
+        window.location.reload();
     }
 
     handleFileChange = (e) => {
--- server.js
+++ server.js
@@ -21,6 +21,9 @@ const connection = mysql.createConnection({
 
 connection.connect();
 
+const multer = require('multer');
+const upload = multer({ dest: './upload' })
+
 app.get('/api/customers', (req, res) => {
     connection.query(
         "SELECT * FROM customer",
@@ -29,5 +32,23 @@ app.get('/api/customers', (req, res) => {
         }
     )
 })
+
+app.use('/image', express.static('./upload'));
+
+app.post('/api/customers', upload.single('image'), (req, res) => {
+    let sql = 'INSERT INTO CUSTOMER VALUES (null, ?, ?, ?, ?, ?)';
+    let image = 'http://localhost:5000/image/' + req.file.filename
+    let userName = req.body.userName;
+    let birthday = req.body.birthday;
+    let gender = req.body.gender;
+    let job = req.body.job;
+    console.log(image + userName + birthday + gender + job);
+    let params = [image, userName, birthday, gender, job];
+    connection.query(sql, params,
+        (err, rows, fields) => {
+            res.send(rows);
+        }
+    );
+});
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
